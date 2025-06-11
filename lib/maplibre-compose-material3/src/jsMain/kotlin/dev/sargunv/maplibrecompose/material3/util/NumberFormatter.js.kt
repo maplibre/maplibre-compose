846712deac0aad56785f60806bcd9ea66423c22f
Sargun Vohra <sargunv@users.noreply.github.com>
2025-06-11 15:44:23 -0700

    fixes

--- lib/maplibre-compose-material3/src/jsMain/kotlin/dev/sargunv/maplibrecompose/material3/util/NumberFormatter.js.kt
+++ lib/maplibre-compose-material3/src/jsMain/kotlin/dev/sargunv/maplibrecompose/material3/util/NumberFormatter.js.kt
@@ -1,14 +1,16 @@
 package dev.sargunv.maplibrecompose.material3.util
 
 import androidx.compose.ui.text.intl.Locale
+import js.intl.NumberFormat
+import js.intl.NumberFormatOptions
 
 internal actual class NumberFormatter
 actual constructor(locale: Locale, maximumFractionDigits: Int) {
 
-  val format =
+  private val format =
     NumberFormat(
       locales = locale.toLanguageTag(),
-      numberFormatOptions = NumberFormatOptions(maximumFractionDigits = maximumFractionDigits),
+      options = NumberFormatOptions(maximumFractionDigits = maximumFractionDigits.coerceAtMost(20)),
     )
 
   actual fun format(value: Number): String = format.format(value)
