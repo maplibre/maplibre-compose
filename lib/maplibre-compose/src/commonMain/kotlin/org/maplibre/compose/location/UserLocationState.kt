b895cadc73c43959899d999a10e7707939f312bf
Klemens Böswirth <me@kodebach.com>
2025-10-13 14:22:59 +0200

    fix: expose lifecycle args in rememberUserLocationState

--- lib/maplibre-compose/src/commonMain/kotlin/org/maplibre/compose/location/UserLocationState.kt
+++ lib/maplibre-compose/src/commonMain/kotlin/org/maplibre/compose/location/UserLocationState.kt
@@ -4,14 +4,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 public class UserLocationState internal constructor(locationState: State<Location?>) {
   public val location: Location? by locationState
 }
 
 @Composable
-public fun rememberUserLocationState(geoLocator: GeoLocator): UserLocationState {
-  val locationState = geoLocator.location.collectAsStateWithLifecycle()
+public fun rememberUserLocationState(
+  geoLocator: GeoLocator,
+  lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+  minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+  coroutineContext: CoroutineContext = EmptyCoroutineContext,
+): UserLocationState {
+  val locationState =
+    geoLocator.location.collectAsStateWithLifecycle(
+      lifecycleOwner = lifecycleOwner,
+      minActiveState = minActiveState,
+      context = coroutineContext,
+    )
   return remember(locationState) { UserLocationState(locationState) }
 }
