811f45950703dc01efb6a567f24859d0305dd5e8
Klemens Böswirth <me@kodebach.com>
2025-10-21 15:47:18 +0200

    doc: document DesiredAccuracy

--- lib/maplibre-compose/src/commonMain/kotlin/org/maplibre/compose/location/GeoLocator.kt
+++ lib/maplibre-compose/src/commonMain/kotlin/org/maplibre/compose/location/GeoLocator.kt
@@ -11,11 +11,56 @@ public interface GeoLocator {
   public val location: StateFlow<Location?>
 }
 
+/**
+ * Accuracy levels for [rememberDefaultGeoLocator], which will be mapped to platform accuracy and
+ * power levels.
+ *
+ * The recommended level for actively displaying the user location on screen is [High] or
+ * [Balanced], if the location doesn't have to be perfectly accurate.
+ */
 public enum class DesiredAccuracy {
+  /**
+   * Request highest possible accuracy and accept high power usage
+   *
+   * On Android this is equivalent to [High]; on iOS it corresponds to "best for navigation apps".
+   * Prefer [High] unless you know you need [Highest].
+   */
   Highest,
+
+  /**
+   * Request very high accuracy and accept high power usage
+   *
+   * On Android this requests the highest possible level of accuracy (same as [Highest]). On iOS
+   * this requests very high accuracy, suitable for everything but real-time navigation.
+   */
   High,
+
+  /**
+   * Request a balance of accuracy and power usage.
+   *
+   * On Android this lets the platform optimize power usage while still requesting reasonably
+   * accurate locations. On iOS this corresponds to an accuracy of roughly 100 m.
+   */
   Balanced,
+
+  /**
+   * Request a rough location with low power usage.
+   *
+   * On Android this is the lowest level that still results in an active location requests, and
+   * therefore the lowest level that guarantees regular location updates. The updates will, however,
+   * be optimized for low power usage and not for accuracy. On iOS this corresponds to an accuracy
+   * of roughly 1 km.
+   */
   Low,
+
+  /**
+   * Request any location with the lowest possible power usage without regard for accuracy.
+   *
+   * On Android this corresponds to the passive provider, i.e. there won't be an active request for
+   * location updates. Instead the app will only receive updates, when another app also requests
+   * location updates. On iOS this corresponds to the reduced accuracy level that is also available
+   * when the app does not have permission for precise locations.
+   */
   Lowest,
 }
 
@@ -45,7 +90,7 @@ public class PermissionException : Exception()
 @Composable
 public expect fun rememberDefaultGeoLocator(
   updateInterval: Duration = 1.seconds,
-  desiredAccuracy: DesiredAccuracy = DesiredAccuracy.Highest,
+  desiredAccuracy: DesiredAccuracy = DesiredAccuracy.High,
   minDistanceMeters: Double = 1.0,
 ): GeoLocator
 
