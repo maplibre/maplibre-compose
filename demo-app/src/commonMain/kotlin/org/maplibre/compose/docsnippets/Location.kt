4d9402b5f0e4649f7196de791d81f1fcde43dc94
Jakob Gillich <jakob@gillich.me>
2026-02-03 14:47:07 +0100

    basic location docs

---
+++ demo-app/src/commonMain/kotlin/org/maplibre/compose/docsnippets/Location.kt
@@ -0,0 +1,43 @@
+package org.maplibre.compose.docsnippets
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.maplibre.compose.camera.CameraPosition
+import org.maplibre.compose.camera.rememberCameraState
+import org.maplibre.compose.location.LocationPuck
+import org.maplibre.compose.location.LocationTrackingEffect
+import org.maplibre.compose.location.mostAccurateBearing
+import org.maplibre.compose.location.rememberDefaultLocationProvider
+import org.maplibre.compose.location.rememberDefaultOrientationProvider
+import org.maplibre.compose.location.rememberUserLocationState
+import org.maplibre.compose.map.MaplibreMap
+
+@Composable
+@OptIn(ExperimentalResourceApi::class)
+fun Location() {
+  // -8<- [start:puck]
+  val cameraState = rememberCameraState()
+
+  val locationProvider = rememberDefaultLocationProvider()
+  val orientationProvider =
+    rememberDefaultOrientationProvider() // optional: get device orientation from sensors
+  val locationState = rememberUserLocationState(locationProvider, orientationProvider)
+
+  MaplibreMap(cameraState = cameraState) {
+    LocationPuck(
+      idPrefix = "user",
+      location = locationState.location,
+      // optional: combine course and orientation bearing
+      bearing = locationState.mostAccurateBearing(),
+      cameraState = cameraState,
+    )
+
+    LocationTrackingEffect(locationState = locationState) {
+      val position = currentLocation.location?.position?.value
+      if (position != null) {
+        cameraState.animateTo(CameraPosition(target = position, zoom = 15.0))
+      }
+    }
+  }
+  // -8<- [end:puck]
+}
