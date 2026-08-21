0bcded051ffaf52673e236aa02aded66c4e1edd6
Sargun Vohra <sargunv@users.noreply.github.com>
2026-08-20 23:59:47 -0400

    Add ColorReliefLayer

---
+++ lib/maplibre-compose/src/nextCommonMain/kotlin/org/maplibre/compose/layers/ColorReliefLayer.kt
@@ -0,0 +1,25 @@
+package org.maplibre.compose.layers
+
+import org.maplibre.compose.expressions.ast.CompiledExpression
+import org.maplibre.compose.expressions.value.ColorValue
+import org.maplibre.compose.expressions.value.FloatValue
+import org.maplibre.compose.sources.Source
+
+internal actual class ColorReliefLayer actual constructor(id: String, actual val source: Source) :
+  Layer(id) {
+
+  override val type: String = "color-relief"
+
+  override val sourceId: String = source.id
+
+  override val sourceDescriptor: Source
+    get() = source
+
+  actual fun setColorReliefColor(color: CompiledExpression<ColorValue>) {
+    setPaintProperty("color-relief-color", color)
+  }
+
+  actual fun setColorReliefOpacity(opacity: CompiledExpression<FloatValue>) {
+    setPaintProperty("color-relief-opacity", opacity)
+  }
+}
