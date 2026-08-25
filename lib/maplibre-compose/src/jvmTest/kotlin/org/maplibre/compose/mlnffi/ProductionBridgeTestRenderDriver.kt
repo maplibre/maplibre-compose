aaca90107941707342c33052befcb7b7d31788c1
Sargun Vohra <sargunv@users.noreply.github.com>
2026-08-25 15:58:10 -0700

    Bump compose-glfw to 0.3.0 (#1096)

--- lib/maplibre-compose/src/jvmTest/kotlin/org/maplibre/compose/mlnffi/ProductionBridgeTestRenderDriver.kt
+++ lib/maplibre-compose/src/jvmTest/kotlin/org/maplibre/compose/mlnffi/ProductionBridgeTestRenderDriver.kt
@@ -586,6 +586,7 @@ private fun interface GlProcAddressCallbackI : CallbackI {
   companion object {
     val DESCRIPTOR =
       Callback.Descriptor(
+        GlProcAddressCallbackI::class.java,
         MethodHandles.lookup(),
         apiCreateCIF(ffi_type_pointer, ffi_type_pointer, ffi_type_pointer),
       )
