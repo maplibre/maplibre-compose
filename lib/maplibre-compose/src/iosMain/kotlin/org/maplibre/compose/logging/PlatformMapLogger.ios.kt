0fd35ac13d83bd25676702c4b443a09e316ae655
Sargun Vohra <sargunv@users.noreply.github.com>
2026-09-02 20:54:07 -0700

    Use the object placeholder for the iOS log sink

--- lib/maplibre-compose/src/iosMain/kotlin/org/maplibre/compose/logging/PlatformMapLogger.ios.kt
+++ lib/maplibre-compose/src/iosMain/kotlin/org/maplibre/compose/logging/PlatformMapLogger.ios.kt
@@ -4,5 +4,5 @@ import platform.Foundation.NSLog
 
 internal actual fun platformMapLogger(): MapLogger = MapLogger { record ->
   val trace = record.throwable?.let { "\n" + it.stackTraceToString() }.orEmpty()
-  NSLog("%s", "[${record.level}] ${record.toPlatformLine()}$trace")
+  NSLog("%@", "[${record.level}] ${record.toPlatformLine()}$trace")
 }
