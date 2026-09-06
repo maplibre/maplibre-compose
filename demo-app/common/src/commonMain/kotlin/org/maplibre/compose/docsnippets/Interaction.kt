e90b71b16cf324e2adfabab5d935497a6f459efc
Sargun Vohra <sargunv@users.noreply.github.com>
2026-09-06 01:31:40 -0700

    docs: focus the interaction guide on common tasks

---
+++ demo-app/common/src/commonMain/kotlin/org/maplibre/compose/docsnippets/Interaction.kt
@@ -0,0 +1,61 @@
+@file:Suppress("unused")
+
+package org.maplibre.compose.docsnippets
+
+import androidx.compose.runtime.Composable
+import org.maplibre.compose.map.KeyModifier
+import org.maplibre.compose.map.MapInteractions
+import org.maplibre.compose.map.MaplibreMap
+import org.maplibre.compose.map.ModifierMatch.Containing
+import org.maplibre.compose.util.ClickResult
+import org.maplibre.spatialk.geojson.Position
+
+@Composable
+fun Interaction() {
+  // #region camera-movement
+  MaplibreMap(
+    interactions =
+      MapInteractions {
+        camera {
+          rotate { enabled = false }
+          tilt { enabled = false }
+        }
+      }
+  )
+  // #endregion camera-movement
+
+  // #region scroll-mappings
+  MaplibreMap(
+    interactions =
+      MapInteractions {
+        bindings {
+          scroll {
+            mappings {
+              on(modifiers = Containing(KeyModifier.Ctrl)) { zoom() }
+              otherwise { pan() }
+            }
+          }
+        }
+      }
+  )
+  // #endregion scroll-mappings
+}
+
+// #region map-click
+@Composable
+fun ClickableMap(onLocationSelected: (Position) -> Unit) {
+  MaplibreMap(
+    interactions =
+      MapInteractions {
+        callbacks {
+          click {
+            onEvent { event ->
+              event.position?.let(onLocationSelected)
+              ClickResult.Consume
+            }
+          }
+        }
+      }
+  )
+}
+// #endregion map-click
